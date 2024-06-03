b57ffd11bd0764e3d2603817630b86f060f6e77d
Mayur2808 <109354866+Mayur2808@users.noreply.github.com>
2024-06-03 13:22:05 +0530

    mm

--- demo/awsadmin.tf
+++ demo/awsadmin.tf
@@ -10,10 +10,10 @@ terraform {
   }
 }
 
-# resource "aws_key_pair" "my_key_pair" {
-#   key_name   = "my-key-pair"
-#   public_key = file("~/.ssh/id_rsa.pub") # Path to your public key
-# }
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "dockerfile1"
+  public_key = file("~/.ssh/id_rsa.pub") # Path to your public key
+}
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-00beae93a2d981137" # Replace with your preferred AMI ID
