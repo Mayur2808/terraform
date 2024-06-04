a9e1e76af83343878134a89d40bf1d5c5d76514a
Mayur2808 <109354866+Mayur2808@users.noreply.github.com>
2024-06-04 13:04:33 +0530

    wey

--- main.tf
+++ main.tf
@@ -2,24 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "vpc" {
-    source = "./module/vpc"
-    this_vpc_cidr_block = "12.11.0.0/16"
-    this_vpc_tags = "this_vpc"
-    this_subnet_pub_cidr_block = "12.11.0.0/17"
-    this_subnet_private1_cidr_block =  "12.11.128.0/19"
-    this_subnet_pub_map_ip = true
-    this_subnet_private_map_ip = false
-    this_subnet_pub_tags  = "pub_subnet"
-    this_subnet_private1_tags = "pri_subnet1"
-    this_subnet_private2_cidr_block = "12.11.192.0/20"
-    this_subnet_private2_tags = "pri_subnet2"
-    this_igw_tags = "igw"
-    this_def_Route_example_cidr_block = "0.0.0.0/0"
-    this_def_Route_example_tag = "default" 
-} 
+# module "vpc" {
+#     source = "./module/vpc"
+#     this_vpc_cidr_block = "12.11.0.0/16"
+#     this_vpc_tags = "this_vpc"
+#     this_subnet_pub_cidr_block = "12.11.0.0/17"
+#     this_subnet_private1_cidr_block =  "12.11.128.0/19"
+#     this_subnet_pub_map_ip = true
+#     this_subnet_private_map_ip = false
+#     this_subnet_pub_tags  = "pub_subnet"
+#     this_subnet_private1_tags = "pri_subnet1"
+#     this_subnet_private2_cidr_block = "12.11.192.0/20"
+#     this_subnet_private2_tags = "pri_subnet2"
+#     this_igw_tags = "igw"
+#     this_def_Route_example_cidr_block = "0.0.0.0/0"
+#     this_def_Route_example_tag = "default" 
+# } 
 
-/*
+
 module "ec2" {
     source = "./module/ec2"
     this_aws_instance_ami = "ami-0eb5115914ccc4bc2"
@@ -35,4 +35,4 @@ module "ec2" {
     this_sg_vpc_id = module.vpc.vpc_id
     //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
 }
-*/
+
